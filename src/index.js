d5c2d8d940ddb94bd2c353c05054037578dac160
Haruka Nishio <haru.nishi.314@gmail.com>
2022-09-10 13:23:44 +0900

    2022-09-10 トップページ 作品一覧用のカードをmuiで作成 * 画像設置はまだできていない * カードを繰り返し処理で複数設置できるようにすることは未実装

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './Component/Header';
+import ActionAreaCard from './Component/Card'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <ActionAreaCard />
   </React.StrictMode>
 );
 
